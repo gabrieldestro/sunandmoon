16ac17b959464f7afd585850e94a7d37f765b49c
gdestro <gabrielaugustodestro@gmail.com>
2024-03-09 10:40:32 -0300

    fixing anims

--- js/cardsLoader.js
+++ js/cardsLoader.js
@@ -2,7 +2,7 @@ async function loadGalery()
 {
     for (var i = 1; i < 83; i++) {
         var name = String(i).padStart(3, '0');
-        $('#galery-row').append("<div class='fadeInUp col-sm-12 col-md-4 col-lg-2 m-auto'><img alt='" + name + "' class='zoom' style='width: 100%; margin: 1rem 0 1rem 0;' src='files/cards/" + name + ".png'/></div>");
+        $('#gallery-row').append("<div class='fadeInUp col-sm-12 col-md-4 col-lg-2 m-auto'><img alt='" + name + "' class='zoom' style='width: 100%; margin: 1rem 0 1rem 0;' src='files/cards/" + name + ".png'/></div>");
         //await sleep(50)
     }
 }
